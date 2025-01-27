eb37d47b30be167fc9dbd756e7f67d274a268e94
Vinícius Santos <santos.viniciusmt@gmail.com>
2025-01-27 16:40:19 -0300

    feat: backend azurerm, tfsate

--- provider.tf
+++ provider.tf
@@ -5,9 +5,20 @@ terraform {
       version = "4.16.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "vinisantosterraformstate"
+    container_name       = "remote-state"
+    key                  = "infra-azure-terraform/terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
   subscription_id = "91f236ff-75a5-49b8-a832-890a54df1390"
 }
